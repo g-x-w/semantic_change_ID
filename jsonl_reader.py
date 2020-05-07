36f21437d1af1cf7c5a6a70d5f5fc4bde58352a1
Gary Wei <gary.wei@mail.utoronto.ca>
2020-05-07 01:07:04 -0400

    First code commit, apparently dataset didn't contain publish dates for sources

---
+++ jsonl_reader.py
@@ -0,0 +1,15 @@
+import json
+
+
+count = 0
+limit = 1
+lines = []
+
+with open('aylien_data.jsonl') as datastream:
+    for i in range(30):
+        lines.append(datastream.readline())
+
+print("\n\n",json.loads(lines[29]))
+print("\n",json.loads(lines[29]).keys())
+# print("\n", (json.loads(lines[8])['published_at']))
+print('\n\nDONE')
